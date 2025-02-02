1f399437f676fa5f93a3a5dc24ad9baf263aff74
anishdasncu <anish20.das06@gmail.com>
2025-02-02 12:59:34 +0530

    Add files via upload

---
+++ c++  question 1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int n;
+
+  int i;
+
+  cout<<"enter a number:"<<endl;
+  
+  cin>>n;
+
+
+
+  for(int i=1;i<n;i++){
+    if(n%i==0){
+        
+        cout<<"Find all the factors of the given number:"<<i<<endl;
+
+    }
+
+  }
+
+  
+    
+
+
+  
+  return 0;
+}
+
+
+
+
+
+
+    
+
+    
+
+ 
+
+
+
