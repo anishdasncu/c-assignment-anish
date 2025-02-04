7a8e12c66a57a4cdcad634e0bed104c35bb19082
anishdasncu <anish20.das06@gmail.com>
2025-02-04 20:21:58 +0530

    Add files via upload

---
+++ Q2 a part.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int arr[]={5,10,15,18,25};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=n-1;i>=0;i--){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
+     
+
+
+
+    
+
+   
+
+
+
+
+
+
+    
+
+
+
+
+        
+
+    
+
+
+
+    
